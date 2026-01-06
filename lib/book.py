b42c1db52329d2076b403eca32cd6153be5f0c28
Jamie Bryson <jamieab98@gmail.com>
2026-01-06 18:34:48 -0500

    completed the book.py file

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, page_Count):
+        self.title = title
+        self.page_Count = page_Count
+        print(f"You are on page {self.page_Count} of {self.title}")
+    
+    def turn_page(self):
+        self.page_Count += 1
+        print(f"You are now on page {self.page_Count} of {self.title}")
     pass
     
-        
+Book1 = Book("A court of thorns and roses", 49)
+Book1.turn_page()
