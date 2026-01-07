d5d2175806c0d79de1477ff1ab1f3ce8364221b3
Jamie Bryson <jamieab98@gmail.com>
2026-01-06 19:01:24 -0500

    Fixed the book.py

--- lib/book.py
+++ lib/book.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self, title, page_Count):
+    def __init__(self, title, page_count):
         self.title = title
-        self.page_Count = page_Count
-        print(f"You are on page {self.page_Count} of {self.title}")
-    
+        if not isinstance(page_count, int):
+            print("page_count is not an integer")
+        else:
+            self.page_count = page_count
+
     def turn_page(self):
-        self.page_Count += 1
-        print(f"You are now on page {self.page_Count} of {self.title}")
+        print("Flipping the page...wow, you read fast!")
+    
     pass
     
-Book1 = Book("A court of thorns and roses", 49)
+Book1 = Book("A court of thorns and roses", 10)
 Book1.turn_page()
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class Coffee:
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+    
     pass
