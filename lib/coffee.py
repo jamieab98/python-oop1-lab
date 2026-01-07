2d4184ce8550148e719d125d9123909b097f7dae
Jamie Bryson <jamieab98@gmail.com>
2026-01-06 19:09:22 -0500

    Completed the coffee file

--- lib/coffee.py
+++ lib/coffee.py
@@ -2,7 +2,13 @@
 
 class Coffee:
     def __init__(self, size, price):
-        self.size = size
         self.price = price
+        if size not in ("Small", "Medium", "Large"):
+            print("size must be Small, Medium, or Large")
+        else:
+            self.size = size
     
+    def tip(self):
+        print("This coffee is great, here's a tip!")
+        self.price += 1    
     pass
